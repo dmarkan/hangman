f8e2ff0f34a720c507b28da88438bee3ea0d10e2
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-01-22 22:50:52 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,166 @@
+let dugmeA = document.querySelector(".dugmeA");
+let dugmeB = document.querySelector(".dugmeB");
+let dugmeC = document.querySelector(".dugmeC");
+let dugmeD = document.querySelector(".dugmeD");
+let dugmeE = document.querySelector(".dugmeE");
+let dugmeF = document.querySelector(".dugmeF");
+let dugmeG = document.querySelector(".dugmeG");
+let dugmeH = document.querySelector(".dugmeH");
+let dugmeI = document.querySelector(".dugmeI");
+let dugmeJ = document.querySelector(".dugmeJ");
+let dugmeK = document.querySelector(".dugmeK");
+let dugmeL = document.querySelector(".dugmeL");
+let dugmeM = document.querySelector(".dugmeM");
+let dugmeN = document.querySelector(".dugmeN");
+let dugmeO = document.querySelector(".dugmeO");
+let dugmeP = document.querySelector(".dugmeP");
+let dugmeQ = document.querySelector(".dugmeQ");
+let dugmeR = document.querySelector(".dugmeR");
+let dugmeS = document.querySelector(".dugmeS");
+let dugmeT = document.querySelector(".dugmeT");
+let dugmeU = document.querySelector(".dugmeU");
+let dugmeV = document.querySelector(".dugmeV");
+let dugmeW = document.querySelector(".dugmeW");
+let dugmeX = document.querySelector(".dugmeX");
+let dugmeY = document.querySelector(".dugmeY");
+let dugmeZ = document.querySelector(".dugmeZ");
+let slovoP = document.querySelector("#slovoP");
+let slovoI = document.querySelector("#slovoI");
+let slovoR = document.querySelector("#slovoR");
+let slovoA = document.querySelector("#slovoA");
+let slovoT = document.querySelector("#slovoT");
+let slovoE = document.querySelector("#slovoE");
+let slovoS = document.querySelector("#slovoS");
+let slovoO = document.querySelector("#slovoO");
+let slovoF = document.querySelector("#slovoF");
+let slovoT2 = document.querySelector("#slovoT2");
+let slovoH = document.querySelector("#slovoH");
+let slovoE2 = document.querySelector("#slovoE2");
+let slovoC = document.querySelector("#slovoC");
+let slovoA2 = document.querySelector("#slovoA2");
+let slovoR2 = document.querySelector("#slovoR2");
+let slovoI2 = document.querySelector("#slovoI2");
+let slovoB = document.querySelector("#slovoB");
+let slovoB2 = document.querySelector("#slovoB2");
+let slovoE3 = document.querySelector("#slovoE3");
+let slovoA3 = document.querySelector("#slovoA3");
+let slovoN = document.querySelector("#slovoN");
+let glava = document.querySelector("#glava");
+let telo = document.querySelector("#telo");
+let rukaD = document.querySelector("#rukaD");
+let rukaL = document.querySelector("#rukaL");
+let nogaD = document.querySelector("#nogaD");
+let nogaL = document.querySelector("#nogaL");
+
+dugmeA.addEventListener("click", function() {
+slovoA.style.visibility = "visible";
+slovoA2.style.visibility = "visible";
+slovoA3.style.visibility = "visible";
+dugmeA.style.visibility = "hidden";
+})
+dugmeB.addEventListener("click", function() {
+slovoB.style.visibility = "visible";
+slovoB2.style.visibility = "visible";
+dugmeB.style.visibility = "hidden";
+})
+dugmeC.addEventListener("click", function() {
+slovoC.style.visibility = "visible";
+dugmeC.style.visibility = "hidden";
+})
+dugmeD.addEventListener("click", function() {
+glava.style.visibility = "visible";
+dugmeD.style.visibility = "hidden";
+})
+dugmeE.addEventListener("click", function() {
+slovoE.style.visibility = "visible";
+slovoE2.style.visibility = "visible";
+slovoE3.style.visibility = "visible";
+dugmeE.style.visibility = "hidden";
+})
+dugmeF.addEventListener("click", function() {
+slovoF.style.visibility = "visible";
+dugmeF.style.visibility = "hidden";
+})
+dugmeG.addEventListener("click", function() {
+telo.style.visibility = "visible";
+dugmeG.style.visibility = "hidden";
+})
+dugmeH.addEventListener("click", function() {
+slovoH.style.visibility = "visible";
+dugmeH.style.visibility = "hidden";
+})
+dugmeI.addEventListener("click", function() {
+slovoI.style.visibility = "visible";
+slovoI2.style.visibility = "visible";
+dugmeI.style.visibility = "hidden";
+})
+dugmeJ.addEventListener("click", function() {
+rukaD.style.visibility = "visible";
+dugmeJ.style.visibility = "hidden";
+})
+dugmeK.addEventListener("click", function() {
+rukaL.style.visibility = "visible";
+dugmeK.style.visibility = "hidden";
+})
+dugmeL.addEventListener("click", function() {
+nogaD.style.visibility = "visible";
+dugmeL.style.visibility = "hidden";
+})
+dugmeM.addEventListener("click", function() {
+nogaL.style.visibility = "visible";
+dugmeM.style.visibility = "hidden";
+})
+dugmeN.addEventListener("click", function() {
+slovoN.style.visibility = "visible";
+dugmeN.style.visibility = "hidden";
+})
+dugmeO.addEventListener("click", function() {
+slovoO.style.visibility = "visible";
+dugmeO.style.visibility = "hidden";
+})
+dugmeP.addEventListener("click", function() {
+slovoP.style.visibility = "visible";
+dugmeP.style.visibility = "hidden";
+})
+dugmeQ.addEventListener("click", function() {
+glava.style.visibility = "visible";
+dugmeQ.style.visibility = "hidden";
+})
+dugmeR.addEventListener("click", function() {
+slovoR.style.visibility = "visible";
+slovoR2.style.visibility = "visible";
+dugmeR.style.visibility = "hidden";
+})
+dugmeS.addEventListener("click", function() {
+slovoS.style.visibility = "visible";
+dugmeS.style.visibility = "hidden";
+})
+dugmeT.addEventListener("click", function() {
+slovoT.style.visibility = "visible";
+slovoT2.style.visibility = "visible";
+dugmeT.style.visibility = "hidden";
+})
+dugmeU.addEventListener("click", function() {
+telo.style.visibility = "visible";
+dugmeU.style.visibility = "hidden";
+})
+dugmeV.addEventListener("click", function() {
+rukaD.style.visibility = "visible";
+dugmeV.style.visibility = "hidden";
+})
+dugmeW.addEventListener("click", function() {
+rukaL.style.visibility = "visible";
+dugmeW.style.visibility = "hidden";
+})
+dugmeX.addEventListener("click", function() {
+nogaL.style.visibility = "visible";
+dugmeX.style.visibility = "hidden";
+})
+dugmeY.addEventListener("click", function() {
+nogaD.style.visibility = "visible";
+dugmeY.style.visibility = "hidden";
+})
+dugmeZ.addEventListener("click", function() {
+glava.style.visibility = "visible";
+dugmeZ.style.visibility = "hidden";
+})
